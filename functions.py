5a9bac5e642b05ca269c978d0881677756f7f638
Ruicky8 <ruizgonzalezunai@gmail.com>
2020-10-01 22:40:56 +0200

    V0.2
    Fixed bugs
    - Input of characters, strings
    - Input of any move outside the table
    - Input in an already full column

--- functions.py
+++ functions.py
@@ -44,57 +44,38 @@ def check_winner(A):
 			or  A[i,j] == A[i+1,j+1] == A[i+2,j+2] == A[i+3,j+3] == 2):
 				keepPlaying = who_wins(A,i,j)
 	return keepPlaying
-	
-def check_errors(A, test):
-	#Check if play is a string or a blank input
-	try:
-		int(test)+1
-		test_1 = 1
-	except ValueError:
-		if test == 'q':
-			return 2
+
+def move(A, player_number):
+	while True:
+		try:
+			if player_number == 1:
+				print("\nFirst player,")
+			else:
+				print("\nSecond player,")
+			play = input("Where do you wanna play?\nEnter 'q' to quit\n")
+			if (int(play) > 7 or int(play) < 1):
+				os.system('cls')
+				print("That move is out of the table!\n")
+				show_table(A)
+				continue
+			if (A[0,int(play) - 1] != 0):
+				os.system('cls')
+				print("That column is already full!\n")
+				show_table(A)
+				continue
+		except ValueError:
+			if (play == 'q'):
+				return play
+			else:
+				os.system('cls')
+				print("That's not a number!\n")
+				show_table(A)
+				continue
 		else:
-			os.system('cls')
-			print("That's not a number!")
-			return 0
-	#Check if play is 0
-	try:
-		1/int(test)
-		test_2 = 1
-	except ZeroDivisionError:
-		os.system('cls')
-		print("That move is out of the table!")
-		return 0
-	#Check if play is a negative number
-	try:
-		math.sqrt(int(test))
-		test_3 = 1
-	except ValueError:
-		os.system('cls')
-		print("That move is out of the table!")
-		return 0
-	#Check if a is between 1 and 7
-	try:
-		B = np.zeros((1,7))
-		j = int(test) - 1
-		B[0,j] = 1
-		test_4 = 1
-	except IndexError:
-		os.system('cls')
-		print("That move is out of the table!")
-		return 0
-	#Check if the column is already full
-	if A[0,int(test)-1] != 0:
-		os.system('cls')
-		print("That column is already full!")
-		return 0 
-	else:
-		test_5 = 1
-	if test_1 == test_2 == test_3 == test_4 == test_5 == 1:
-		return 1
+			return play
 			
 def player_move(A, player_number, play):
-	#Since we are choosing between 1 and 7 and arrays start in 0 we have to add 1
+	#Since we are choosing between 1 and 7 and arrays start in 0 we have to substract 1
 	j = int(play) - 1
 	#j will be the column the player wanna play
 	for i in range(5,-1,-1):
@@ -109,19 +90,10 @@ def player_move(A, player_number, play):
 
 def player(A, player_number):
 	keepPlaying = True
-	if player_number == 1:
-		print("\nFirst player,")
+	play = move(A, player_number)
+	if play == 'q':
+		#Returns keepPlaying
+		return False
 	else:
-		print("\nSecond player,")
-	play = input("Where do you wanna play?\nEnter 'q' to quit\n")
-	error = check_errors(A, play)
-	if error == 1:
 		keepPlaying = player_move(A, player_number, play)
-		return keepPlaying
-	elif error == 0:	
-		print("Try again...\n")
-		show_table(A)
-		player(A, player_number)
-	elif error == 2:
-		keepPlaying = False
 		return keepPlaying
